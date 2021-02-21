617bc77970a47bfaf2c531514dbd520d07fdf3b0
Jesus Ramirez <32681573+lJeskl@users.noreply.github.com>
2021-02-20 21:51:01 -0500

    Add files via upload

---
+++ JavaScript Objects Project/MyGame/MyGame.js
@@ -0,0 +1,18 @@
+function MyGame(htmlCanvasID) {
+    // The shader for drawing
+    this.mShader = null;
+
+    gEngine.Core.initializeWebGL(htmlCanvasID);
+    //this.mShader = new SimpleShader("VertexShader", "FragmentShader");
+    this.mShader = new SimpleShader(
+        "GLSLShaders/SimpleVS.glsl", // Path to the VertexShader
+        "GLSLShaders/SimpleFS.glsl"); // Path to the FragmentShader
+    // Step C1: Clear the canvas
+    gEngine.Core.clearCanvas([0, 0.8, 0, 1]);
+    
+    // Step C2: Activate the proper shader
+    this.mShader.activateShader([0,0,1,1]);
+    // Step C3: Draw with the currently activated geometry and the activated shader
+    var gl = gEngine.Core.getGL();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+   }
